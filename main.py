f6e43476e131c4c9fd1e59cb10b185ac24fbae17
dejandrea <dejandrea@gmail.com>
2025-03-29 09:04:14 -0300

    Tela inicial do jogo

--- main.py
+++ main.py
@@ -7,6 +7,10 @@ import random
 WIDTH = 700
 HEIGHT = 700
 GAME_STATE = "PLAY"
+GAME_STATE = "PLAY"
+sounds_on = True
+score = 0
+lives = 3
 
 #background
 background = Actor("background")
@@ -25,7 +29,21 @@ def draw():
     screen.clear()
 
     if GAME_STATE == "START":
-        screen.fill((0, 0, 0))  # Cor de fundo preta
+        screen.fill((0, 0, 0))
+        # Título
+        screen.draw.text("Menu Principal", center=(WIDTH // 2, 100), fontsize=50, color="white", align="center")
+
+        # Botão Começar Jogo
+        screen.draw.rect(Rect((WIDTH / 2 - 100, 200), (200, 50)), color="blue")
+        screen.draw.text("Começar Jogo", center=(WIDTH / 2, 225), fontsize=30, color="white")
+
+        # Botão Ligar/Desligar Sons
+        screen.draw.rect(Rect((WIDTH / 2 - 100, 300), (200, 50)), color="green")
+        screen.draw.text("Sons: On" if sounds_on else "Sons: Off", center=(WIDTH / 2, 325), fontsize=30, color="white")
+
+        # Botão Sair
+        screen.draw.rect(Rect((WIDTH / 2 - 100, 400), (200, 50)), color="red")
+        screen.draw.text("Sair", center=(WIDTH / 2, 425), fontsize=30, color="white")
 
 
     elif GAME_STATE == "PLAY":
@@ -56,8 +74,22 @@ def update():
     if bg_x >= 0:
         bg_x = 0
 
+# Função para verificar cliques do mouse
+def on_mouse_down(pos, button):
+    global sounds_on
 
+    # Verifica se o botão "Começar Jogo" foi clicado
+    if Rect((WIDTH / 2 - 100, 200), (200, 50)).collidepoint(pos):
+        start_game()
     
+    # Verifica se o botão "Ligar/Desligar Sons" foi clicado
+    elif Rect((WIDTH / 2 - 100, 300), (200, 50)).collidepoint(pos):
+        sounds_on = not sounds_on  # Alterna o estado dos sons
+        toggle_sounds()
+
+    # Verifica se o botão "Sair" foi clicado
+    elif Rect((WIDTH / 2 - 100, 400), (200, 50)).collidepoint(pos):
+        exit_game()
 
 
 # Função para iniciar o jogo
@@ -65,8 +97,15 @@ def start_game():
     global GAME_STATE
     GAME_STATE = "PLAY"
     print("Jogo Iniciado!")   
-    
 
+# Função para ligar ou desligar os sons
+def toggle_sounds():
+    #TODO: TROCAR O SOM DE FUNDO E ACRESCENTAR SONS DO JOGO
+    if sounds_on:
+        sounds.air.play(loops=True)  # Retoma a música de fundo
+    else:
+        sounds.air.stop()  # Pausa a música de fundo
+    
 # Função para sair do jogo
 def exit_game():
     print("Saindo do jogo...")
